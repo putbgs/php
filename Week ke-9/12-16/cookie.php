646ff6423db9e97d8467dcad14397147ebd79579
putbgs <67993786+putbgs@users.noreply.github.com>
2021-03-31 21:56:50 +0700

    week 9

---
+++ Week ke-9/12-16/cookie.php
@@ -0,0 +1,19 @@
+<?php
+
+$cookie_name = 'user';
+$cookie_value = 'jhony';
+
+setcookie($cookie_name, $cookie_value);
+
+
+$cookie_value = 'tejo';
+
+setcookie($cookie_name, $cookie_value);
+
+echo $_COOKIE[$cookie_name];
+
+setcookie("user", "", time() - 3600);
+
+echo '<br>';
+
+var_dump($_COOKIE);
